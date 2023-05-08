980fe6187b49ae91babea9e6e9b6ce8f9e6075cc
Osiris Mariano <oslimacontato@gmail.com>
2023-05-08 19:54:07 -0300

    feature: Adiciona arquivo /hero_spec.rb & /hero.rb

---
+++ lib/hero.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+class Hero
+  attr_accessor :weapon
+
+  def initialize
+    @weapon = 'sword'
+  end
+end
---
+++ spec/lib/hero_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+require 'spec_helper'
+require 'hero'
+
+describe Hero do
+  it 'has a sword' do
+    hero = Hero.new
+    expect(hero.weapon).to eq('sword')
+  end
+end
