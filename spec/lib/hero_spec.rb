5c647f8c4e4a9fe43cdeed12ef0e47b89207a1d5
Osiris Mariano <oslimacontato@gmail.com>
2023-05-08 20:10:35 -0300

    feature: Edita arquivo hero_spec.rb

--- spec/lib/hero_spec.rb
+++ spec/lib/hero_spec.rb
@@ -3,8 +3,13 @@ require 'spec_helper'
 require 'hero'
 
 describe Hero do
-  it 'has a sword' do
-    hero = Hero.new
-    expect(hero.weapon).to eq('sword')
+  before(:all)do
+    @hero = Hero.new
+  end
+  it "has a sword" do
+    expect(@hero.weapon).to eq "sword"
+  end
+  it "has more than 1000 HP points" do
+    expect(@hero.hp_point).to be > 1000
   end
 end
